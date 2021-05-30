dff438222d0a1436fabec48347d078f8522b7d20
Favour <ayofavour7@gmail.com>
2021-05-30 16:05:48 +0100

    feat: add error handling

--- frontend/src/views/App.jsx
+++ frontend/src/views/App.jsx
@@ -17,13 +17,21 @@ function App() {
     dispatch(refreshFiles());
   }, POLL_INTERVAL, true);
 
-  const { list, hasError, isLoading } = useSelector(store => store.files.file);
+  const { list, hasError } = useSelector(store => store.files.file);
   const { selectedFile } = useSelector(store => store.filemanager);
 
   return (
-    <div className={styles.App}>
-      <FileExplorer files={list}  />
-      <FileContent file={selectedFile} />
+    <div className={styles.AppContainer}>
+      {
+        hasError ? <div className={styles.Error}>
+          <h3>An error occured while fetching the files</h3>
+        </div> : (
+          <div className={styles.App}>
+            <FileExplorer files={list} />
+            <FileContent file={selectedFile} />
+          </div>
+        )
+      }
     </div>
   );
 }
